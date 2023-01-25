98079083f88923e2a78d7e1eaaf52a3b15aee218
CaramonH <caramonh@yahoo.com>
2023-01-25 09:32:04 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/** 
+ * main - entry point
+ * return - always 0
+ */
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
