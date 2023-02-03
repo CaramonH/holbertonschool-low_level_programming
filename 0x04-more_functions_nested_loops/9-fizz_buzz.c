7cd1135536dfb659cc0ee419a0564c9521913166
CaramonH <caramonh@yahoo.com>
2023-02-03 10:24:02 -0800

    devour

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,29 +6,22 @@
  */
 int main(void)
 {
-	int i;
-	
-	for (i = 1; i <= 100; i++)
+	int n = 1;
+
+	while (n <= 100)
 	{
-		if ((i % 5 == 0) && (i %3) == 0)
-		{
-		printf("FizzBuzz");
-		}
-		else if (i % 5 == 0)
-		{
-		printf("Buzz");
-		}
-		else if (i % 3 == 0)
-		{
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
 			printf("Fizz");
-		}
+		else if (n % 5 == 0)
+			printf("Buzz");
 		else
-		{
-			printf("%i", i);
-		}
-		if (i < 100)
-		printf(" ");
+			printf("%d", n);
+		if (n != 100)
+			printf(" ");
+		n++;
 	}
-printf("\n");
-return (0);
+	printf("\n");
+	return (0);
 }
