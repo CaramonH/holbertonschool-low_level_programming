85e6203a663daf1739ede4aad513daf07285d323
CaramonH <caramonh@yahoo.com>
2023-02-09 10:46:39 -0800

    lets see

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * string_toupper - Change lowercase letters to uppercase in a string
+ * @p: String that will be capitalized
+ * Return: char var
+ */
+char *string_toupper(char *p)
+{
+int i;
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		n[i] = n[i] - 32;
+	}
+	return (n);
+}
