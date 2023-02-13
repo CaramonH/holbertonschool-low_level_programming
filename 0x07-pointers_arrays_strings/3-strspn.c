710c5bc89edbb5f61b53291c61d2ede976121526
CaramonH <caramonh@yahoo.com>
2023-02-13 10:22:01 -0800

    header

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
-/** _strspn - gets the length of a prefix substring
- * @s: source string
- * @accept: accepted string
- * Return: number of bytes in the init segment
+/**
+ * _strspn - get the lenght of prefix substring
+ * @s:string
+ * @accept:string
+ * Return:unsigned int
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int x;
