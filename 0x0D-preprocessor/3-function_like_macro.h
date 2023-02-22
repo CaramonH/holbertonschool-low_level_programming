c3eef4be2e3b6f4cb22600a04a00dc602eb36886
CaramonH <caramonh@yahoo.com>
2023-02-22 12:04:20 -0800

    we dun it

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-TYPEDEF INT MAKE_ISO_COMPILERS_HAPPY;
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
 
 #endif
