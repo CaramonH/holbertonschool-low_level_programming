6d1455ea2133255bb54e273f381951a2e0c1a8b6
CaramonH <caramonh@yahoo.com>
2023-03-01 10:28:01 -0800

    doggy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - Dog attributes
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ *
+ * Description: The attributes of the dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+#endif
