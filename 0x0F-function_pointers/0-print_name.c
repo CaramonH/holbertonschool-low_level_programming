76472f3790e0babc3016ec9e1775c5d31572ee53
CaramonH <caramonh@yahoo.com>
2023-03-02 12:09:51 -0800

    what

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,8 +6,8 @@
  * @f: a pointer to a function that prints a name
  */
 
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL || f != NULL)
+	if (name != NULL && f != NULL)
 		f(name);
 }
