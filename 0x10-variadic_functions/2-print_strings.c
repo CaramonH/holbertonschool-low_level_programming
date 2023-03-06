1dcb8662f9518ed531da47cd0cd1a8e71bb9707b
CaramonH <caramonh@yahoo.com>
2023-03-06 10:41:01 -0800

    willpower

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints string arguments after n
+ * @separator: string to print between strings
+ * @n: number of strings after n
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *s;
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(ap, n);
+
+	for (i = 0; i < n - 1; i++)
+	{
+		s = va_arg(ap, char *);
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+
+		if (separator != NULL)
+			printf("%s", separator);
+	}
+	s = va_arg(ap, char *);
+	if (s == NULL)
+		printf("(nil\n");
+	else
+		printf("%S\n", s);
+}
