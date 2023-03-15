76635b344b03368d187c33cdfbd890bd15640aea
CaramonH <caramonh@yahoo.com>
2023-03-15 10:58:55 -0700

    apostraphe

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,7 +16,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	newNode->str = strdup(str);
 	newNode->len = strlen(str);
-	newNode->next = NULL;'
+	newNode->next = NULL;
 
 	if (*head == NULL)
 		*head = newNode;
