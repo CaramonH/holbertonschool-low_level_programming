af8a2e670eb4f4082da87966c8b125bf4b56a246
CaramonH <caramonh@yahoo.com>
2023-03-16 18:47:09 -0700

    listint

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - a function that frees a listint_t list
+ * @head: inout list
+ */
+void free_listint(listint_t *head);
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+	free(head);
+}
