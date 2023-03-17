31922e241b2e759960b1f2156b70df7c7ccb2e56
CaramonH <caramonh@yahoo.com>
2023-03-17 09:11:26 -0700

    new twitter

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,17 +7,16 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *temp = *head;
-	int data;
+	listint_t *new_head;
+	int n = 0;
 
-	if (!(head))
-		return (0);
+	if (*head != NULL)
+	{
+		new_head = (*head)->next;
+		n = (*head)->n;
+		free(*head);
+		*head = new_head;
+	}
 
-	data = temp->n;
-
-	*head = (*head)->next;
-
-	free(temp);
-
-	return (data);
+	return (n);
 }
