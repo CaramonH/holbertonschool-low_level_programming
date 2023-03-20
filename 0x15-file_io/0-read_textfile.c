f9c8c46c5c3b9b4ca67b09e01c4cb902367187f8
CaramonH <caramonh@yahoo.com>
2023-03-20 12:05:30 -0700

    git

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,37 +8,23 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t bytes_read, bytes_written;
-	char *buffer;
+	int fd, readed;
+	char *buff = malloc(sizeof(char *) * letters);
 
-	if (filename == NULL)
+	if (!buff)
 		return (0);
 
-	buffer = malloc(sizeof(char) * (letters));
-	if (buffer == NULL)
+	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY, 0600);
 	if (fd == -1)
 		return (0);
 
-	bytes_read = read(fd, buffer, letters);
+	readed = read(fd, buff, letters);
+	write(STDOUT_FILENO, buff, readed);
+
+	free(buff);
 	close(fd);
-
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written != bytes_read)
-	{
-		free(buffer);
-		return (0);
-	}
-
-	free(buffer);
-	return (bytes_written);
+	return (readed);
 }
