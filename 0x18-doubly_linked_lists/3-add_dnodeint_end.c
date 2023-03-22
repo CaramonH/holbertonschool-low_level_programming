8d1f5bd3d7842b2fb20aa5581db9f4aa02dcc67e
CaramonH <caramonh@yahoo.com>
2023-03-22 09:53:08 -0700

    really

--- 0x18-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	newNode->next = NULL;
 	newNode->prev = NULL;
 
-	if (*head == NULL)
+	if (!*head)
 	{
 		*head = newNode;
 		return (newNode);
