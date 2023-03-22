3b4a3c49ce8d50931f188e2286c6dff001f2f4fb
CaramonH <caramonh@yahoo.com>
2023-03-22 09:54:26 -0700

    newnode

--- 0x18-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,7 +8,7 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *temp = *head
+	dlistint_t *temp = *head;
 	dlistint_t *newNode = malloc(sizeof(*newNode));
 
 	if (!newNode)
