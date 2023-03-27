43688cdc5fa66943b090ccb4baed14c0680fd8c9
CaramonH <caramonh@yahoo.com>
2023-03-27 10:29:04 -0700

    fake friends

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,52 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to a hash table
+ * @ht: hash table to add to
+ * @key: key to be added
+ * @value: value to be added
+ * Return: 1 on success, else 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *newNode, *curr;
+
+	if (!ht || !key || strcmp(key, "\0") == 0)
+		return (0);
+
+	index = key_index((unsigned char *) key, ht->size);
+	curr = ht->array[index];
+
+	newNode = malloc(sizeof(hash_node_t));
+	if (!newNode)
+		return (0);
+	newNode->key = strdup(key);
+	newNode->value = strdup(value);
+	newNode->next = NULL;
+	if (curr)
+	{
+		if (strcmp(curr->key, key) == 0)
+		{
+			newNode->next = curr;
+			ht->array[index] = newNode;
+			return (1);
+		}
+		while (curr->next)
+		{
+			if (strcmp(curr->next->key, key) == 0)
+			{
+				newNode->next = curr->next->next;
+				free(curr->next->key), free(curr->next->value), free(curr->next);
+				curr->next = newNode;
+				return (1);
+			}
+			curr = curr->next;
+		}
+		newNode->next = ht->array[index];
+		ht->array[index] = newNode;
+		return (1);
+	}
+	ht->array[index] = newNode;
+	return (1);
+}
