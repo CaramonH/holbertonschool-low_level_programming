935a76553c188d71b3d646d29f34913186a2a396
CaramonH <caramonh@yahoo.com>
2023-12-20 10:55:32 -0800

    over 8- characters

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,7 +1,8 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Function that searches for a value in an array of integers using the Linear search algorithm
+ * linear_search - Function that searches for a value in
+ * an array of integers using the Linear search algorithm
  * @array: Pointer to the first element of the array to search in
  * @size: Number of elements in the array
  * @value: Value to search for
